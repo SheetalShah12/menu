3e53e7f5778afd51a907f22736dc8f487213b764
Sheetal Shah <shahsheetal160@gmail.com>
2025-04-20 21:29:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var typed = new Typed(".typing1",{
+    strings:["Appetizers"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typing2",{
+    strings:["Salads"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typing3",{
+    strings:["Chinese"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typing4",{
+    strings:["Main Course"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typing5",{
+    strings:["Desserts"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typing6",{
+    strings:["Beverages"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
